627be9ec3ebdb602bc3bc0d02483559966bffa71
ruzaini Jobstore <ruzaini@jobstore.com>
2021-02-06 21:47:32 +0800

    :zap: add some changes

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,8 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+cd package
+git clone https://github.com/liuran001/openwrt-packages
+git clone https://github.com/destan19/OpenAppFilter.git
+git clone https://github.com/zzsj0928/luci-app-serverchand
+git clone https://github.com/siropboy/luci-theme-btmod 
