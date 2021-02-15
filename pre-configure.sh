17ecc9ae1db4a34d93b55ae74b6e7b8d781968d6
Ruzaini <ruzaini@jobstore.com>
2021-02-15 21:36:34 +0800

    :zap: update some

--- pre-configure.sh
+++ pre-configure.sh
@@ -11,6 +11,19 @@ set -e -x
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+# Modify hostname
+sed -i 's/OpenWrt/OurtechHome/g' package/base-files/files/bin/config_generate
+
+git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/lean/luci-theme-opentomcat
+git clone https://github.com/garypang13/luci-theme-edge.git package/lean/luci-theme-edge
+
+echo '修改wifi名称'
+sed -i 's/OpenWrt/OurtechHome/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+echo '修改banner'
+rm -rf package/base-files/files/etc/banner
+cp -f ../banner package/base-files/files/etc/
+
 curl --retry 5 -L "https://downloads.openwrt.org/releases/$(printf "%s" "$REPO_BRANCH" | cut -c 2-)/targets/ipq40xx/generic/config.buildinfo" > .config
 sed -e '/^CONFIG_TARGET_DEVICE_/d' -e '/CONFIG_TARGET_ALL_PROFILES=y/d' -i .config
 cat "$GITHUB_WORKSPACE/additional_config.txt" >> .config
--- pre-feeds.sh
+++ pre-feeds.sh
@@ -1,5 +1,4 @@
-#!/bin/sh
-set -e -x
+#!/bin/bash
 #
 # Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
 #
@@ -11,4 +10,15 @@ set -e -x
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-true
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+# sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+cd package
+git clone https://github.com/liuran001/openwrt-packages
+git clone https://github.com/destan19/OpenAppFilter.git
+git clone https://github.com/zzsj0928/luci-app-serverchand
+git clone https://github.com/siropboy/luci-theme-btmod 
+git clone https://github.com/kenzok8/small.git package/small
+git clone https://github.com/fw876/helloworld.git package/openwrt-packages/luci-app-ssr-plus
